3dbc05e377ab79348c326430817d94def0093fc8
Azriel Akbar Ferry Ardiansyah Kusumawardhana <ferryakbarardiansyah@gmail.com>
2022-01-13 10:20:57 +0700

    update
    Signed-off-by: Azriel Akbar Ferry Ardiansyah Kusumawardhana <ferryakbarardiansyah@gmail.com>

--- lib/common/controllers/blog_by_id_controller.dart
+++ lib/common/controllers/blog_by_id_controller.dart
@@ -1,3 +1,20 @@
 import 'package:get/get.dart';
+import 'package:kost/model/blog_by_id_model.dart';
 
-class BlogByIdController extends GetxController {}
+class BlogByIdController extends GetxController {
+  final _blog = Blog().obs;
+  final _idBlog = ''.obs;
+
+  //getter
+  Blog get getBlog => _blog.value;
+  String get getIdBlog => _idBlog.value;
+
+  //setter
+  set setBlog(Blog value) => _blog.value = value;
+  set setIdBlog(String value) => _idBlog.value = value;
+
+  @override
+  void onInit() {
+    super.onInit();
+  }
+}
